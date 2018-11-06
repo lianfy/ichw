4551bf559db87452e4ba3b74913183a2bbbdb0c6
Feiyue Lian <2496423641@qq.com>
2018-11-06 22:03:29 +0800

    Update planets.py

--- pyassign1/planets.py
+++ pyassign1/planets.py
@@ -1,46 +1,35 @@
 import turtle
 import math
 a = turtle.Turtle()
-a.color("yellow")
-a.shape("circle")
 b = turtle.Turtle()
-b.color("blue")
-b.shape("circle")
 c = turtle.Turtle()
-c.color("green")
-c.shape("circle")
 d = turtle.Turtle()
-d.color("red")
-d.shape("circle")
 e = turtle.Turtle()
-e.color("black")
-e.shape("circle")
 f = turtle.Turtle()
-f.color("orange")
-f.shape("circle")
 g = turtle.Turtle()
-g.color("lightblue")
-g.shape("circle")
 
 
-def orbit(t, x):
+def orbit(t, c, x):
+    t.color(c)
+    t.shape("circle")
+    t.speed(0)
     t.up()
     t.fd(x)
     t.down()
 
 
 def mov(t, x, y, i):
-     t.goto(x*math.cos(math.radians(4*10**6*i/(x+y)**3)), y*math.sin(math.radians(4*10**6*i/(x+y)**3)))
+    t.goto(x*math.cos(math.radians(4*10**6*i/(x+y)**3)), y*math.sin(math.radians(4*10**6*i/(x+y)**3)))
 
 
-orbit(a, 0)
-orbit(b, 68.32)
-orbit(c, 85.34)
-orbit(d, 100.8)
-orbit(e, 129.1)
-orbit(f, 233.6)
-orbit(g, 318.1)
-for i in range(1500):
+orbit(a, "yellow", 0)
+orbit(b, "blue", 68.32)
+orbit(c, "green", 85.34)
+orbit(d, "red", 100.8)
+orbit(e, "black", 129.1)
+orbit(f, "orange", 233.6)
+orbit(g, "lightblue", 318.1)
+for i in range(20000):
     mov(b, 68.32, 55.45, i)
     mov(c, 85.34, 84.76, i)
     mov(d, 100.8, 99.16, i)
